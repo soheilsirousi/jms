8ff758f00da33737279795545cc31530c7c39567
Seyed Mohammad Soheil Sirousi <soheilsirousi@Seyeds-MacBook-Air.local>
2025-12-19 11:26:27 +0330

    fix image path function url

--- jms/catalogue/utils.py
+++ jms/catalogue/utils.py
@@ -6,4 +6,4 @@ def product_image_upload_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = f"{uuid.uuid4()}.{ext}"
     store_name = slugify(instance.store.name)
-    return f'imait ges/store/{store_name}/products/{filename}'
+    return f'images/store/{store_name}/products/{filename}'
